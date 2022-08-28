65bdc4ba42836f5183d5c3ce8ce352eaab9e8733
Diego Neves <pdcassiano@gmail.com>
2022-08-28 11:33:05 -0300

    jest

--- teste-basico/calcular-valor-pedidos/calcular-valor-pedido.test.js
+++ teste-basico/calcular-valor-pedidos/calcular-valor-pedido.test.js
@@ -1,6 +1,6 @@
 const calcularValorPedido = require('./calcular-valor-pedido')
 
-it('não deve cobrar valor de frete quanto valor dos produtos for superior a 500', () => {
+it('não deve cobrar valor de frete quando valor dos produtos for superior a 500', () => {
     const meuPedido = {
         itens: [
           { nome: "Arco encantado", valor: 2000 },
@@ -10,7 +10,7 @@ it('não deve cobrar valor de frete quanto valor dos produtos for superior a 500
       const resultado = calcularValorPedido(meuPedido)
       expect(resultado).toBe(2000)
 })
-it('deve cobrar valor de frete quanto valor dos produtos for inferior a 500', () => {
+it('deve cobrar valor de frete quando valor dos produtos for inferior a 500', () => {
     const meuPedido = {
         itens: [
           { nome: "poção de vida", valor: 100 },
@@ -19,4 +19,14 @@ it('deve cobrar valor de frete quanto valor dos produtos for inferior a 500', ()
       }
       const resultado = calcularValorPedido(meuPedido)
       expect(resultado).toBe(150)
+})
+it('deve cobrar valor de frete quando valor dos produtos for igual a 500', () => {
+    const meuPedido = {
+        itens: [
+          { nome: "sanduiche bem caro", valor: 500 },
+          { nome: 'Entrega', valor: 50, entrega: true}
+        ],
+      }
+      const resultado = calcularValorPedido(meuPedido)
+      expect(resultado).toBe(550)
 })
