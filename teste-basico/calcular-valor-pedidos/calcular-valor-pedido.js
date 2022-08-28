2778005f5a84c9d405f18e9de0efe1993e929eaa
Diego Neves <pdcassiano@gmail.com>
2022-08-28 11:53:02 -0300

    jest

--- teste-basico/calcular-valor-pedidos/calcular-valor-pedido.js
+++ teste-basico/calcular-valor-pedidos/calcular-valor-pedido.js
@@ -1,8 +1,15 @@
+const acredcimoValorEntrega = 0.2
+
 const calcularValorPedido = (pedido) => {
     const valorProdutos = pedido.itens.filter((item) => !item.entrega)
     .reduce((totalPedidos, pedidoAtual) => totalPedidos + pedidoAtual.valor, 0)
 
     const entrega = pedido.itens.filter(item => item.entrega)
+    
+    if(pedido.estado === 'RS' || pedido.estado === 'SC') {
+        const acrescimoEntrega = entrega[0].valor * acredcimoValorEntrega
+        entrega[0].valor += acrescimoEntrega
+    }
 
     return (valorProdutos> 500) ? valorProdutos : valorProdutos + entrega[0].valor
 }
--- teste-basico/calcular-valor-pedidos/calcular-valor-pedido.test.js
+++ teste-basico/calcular-valor-pedidos/calcular-valor-pedido.test.js
@@ -30,3 +30,40 @@ it('deve cobrar valor de frete quando valor dos produtos for igual a 500', () =>
       const resultado = calcularValorPedido(meuPedido)
       expect(resultado).toBe(550)
 })
+it('Deve adicionar um acrescimo de 20% no valor da entrega do pedido caso o estado seja RS', () => {
+  const pedidoComEstadoRS = {
+    estado: 'RS',
+    itens: [
+      { nome: "sanduiche bem caro", valor: 500 },
+      { nome: 'Entrega', valor: 100, entrega: true}
+    ],
+  }
+  const resultado = calcularValorPedido(pedidoComEstadoRS)
+
+  expect(resultado).toBe(620)
+})
+it('Deve adicionar um acrescimo de 20% no valor da entrega do pedido caso o estado seja SC', () => {
+  const pedidoComEstadoSC = {
+    estado: 'SC',
+    itens: [
+      { nome: "sanduiche bem caro", valor: 500 },
+      { nome: 'Entrega', valor: 100, entrega: true}
+    ],
+  }
+  const resultado = calcularValorPedido(pedidoComEstadoSC)
+
+  expect(resultado).toBe(620)
+})
+
+it('Não deve adicionar um acrescimo de 20% no valor da entrega do pedido caso o estado seja SP', () => {
+  const pedidoComEstadoSP = {
+    estado: 'SP',
+    itens: [
+      { nome: "sanduiche bem caro", valor: 500 },
+      { nome: 'Entrega', valor: 100, entrega: true}
+    ],
+  }
+  const resultado = calcularValorPedido(pedidoComEstadoSP)
+
+  expect(resultado).toBe(600)
+})
