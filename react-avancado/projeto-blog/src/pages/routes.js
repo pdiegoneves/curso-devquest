91380f82449f023967203fe1b3730cb1d2d4d2f0
Diego Neves <pdcassiano@gmail.com>
2022-08-29 18:51:46 -0300

    projeto blog

---
+++ react-avancado/projeto-blog/src/pages/routes.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import { Posts } from './posts'
+
+const AppRoutes  = () => (
+  <BrowserRouter>
+    <Routes>
+      <Route exact path="/" element={<Posts />} />
+    </Routes>
+  </BrowserRouter>
+);
+
+export default AppRoutes;
