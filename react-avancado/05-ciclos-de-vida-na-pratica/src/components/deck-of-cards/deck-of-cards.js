d09b749279b80b22bbe91ef1b7891e342dc07764
Diego Neves <pdcassiano@gmail.com>
2022-08-29 15:56:09 -0300

    deck of cards

---
+++ react-avancado/05-ciclos-de-vida-na-pratica/src/components/deck-of-cards/deck-of-cards.js
@@ -0,0 +1,52 @@
+import { Component } from "react";
+
+async function createDeck() {
+  const response = await fetch(
+    "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1"
+  );
+  const deck = await response.json();
+  return deck.deck_id;
+}
+
+async function getCards(deckId) {
+  const response = await fetch(
+    `https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=52`
+  );
+  return await response.json();
+}
+
+class DeckOfCards extends Component {
+  constructor() {
+    super();
+    this.state = {
+      cards: [],
+    };
+  }
+
+  async componentDidMount() {
+    const deckID = await createDeck();
+    const data = await getCards(deckID);
+
+    this.setState({
+      cards: data.cards,
+    });
+  }
+
+  render() {
+    return (
+      <section>
+        <ul>
+          {this.state.cards.map((card, index) => {
+            return (
+              <li key={index}>
+                <img src={card.image} alt={card.value} />
+              </li>
+            );
+          })}
+        </ul>
+      </section>
+    );
+  }
+}
+
+export default DeckOfCards;
