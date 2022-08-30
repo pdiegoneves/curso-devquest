a4dba10906e939821fe6d6a667359a1af036c89d
Diego Neves <pdcassiano@gmail.com>
2022-08-29 22:24:20 -0300

    routes

--- react-avancado/projeto-blog/src/components/posts/index.js
+++ react-avancado/projeto-blog/src/components/posts/index.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react'
 
 async function getPosts() {
-    const response = await fetch('http://localhost:3001/json/posts.json')
+    const response = await fetch('http://localhost:3000/json/posts.json')
     return await response.json()
 }
 
