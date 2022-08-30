2342821ca4de779712e5305999d31f0cade691ff
Diego Neves <pdcassiano@gmail.com>
2022-08-30 14:29:42 -0300

    redux

---
+++ react-redux/redux-shop-inicial/src/components/header/index.js
@@ -0,0 +1,33 @@
+import styled from "styled-components";
+import { CartProductsCounter } from "../cart-products-counter";
+import { Link } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+
+const Header = () => {
+
+  const cartProducts = useSelector(state => state.cartProducts)
+
+  return (
+    <Container>
+      <Link to="/">
+        <h1>Redux Shop</h1>
+      </Link>
+      <CartProductsCounter />
+    </Container>
+  );
+};
+
+const Container = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: #2C313F;
+  color: #ffffff;
+  padding: 20px 30px;
+
+  a {
+      color: #ffffff;
+  }
+`;
+
+export { Header };
